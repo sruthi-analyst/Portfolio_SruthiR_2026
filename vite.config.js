db876cdbc1036729d3b092980f89888dc66471ea
Sruthi R <sruthi.r.analyst@gmail.com>
2026-06-18 00:09:53 +0530

    Set base path for Vite configuration
    Added base path for deployment.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/Portfolio_SruthiR_2026/', // Add this line
 })
